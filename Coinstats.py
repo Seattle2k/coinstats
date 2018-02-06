1fcea4d9d3f3388f4cdc5cb340471940dfd624ad
Seattle2k <Seattle2k@gmail.com>
2018-02-06 00:22:59 -0800

    Merge branch 'master' of https://github.com/Seattle2k/coinstats
    # Conflicts:
    #	Procfile

--- Coinstats.py
+++ Coinstats.py
@@ -7,12 +7,14 @@ class BotHandler:
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
+        print('GET_UPDATES')
         print(self.api_url + method)
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
         #resp = requests.get(self.api_url + method)
-        print(resp)
         print('resp')
+        print(resp)
+        print('resp2')
         result_json = resp.json()['result']
         return result_json
 
@@ -24,17 +26,19 @@ class BotHandler:
 
     def get_last_update(self):
         get_result = self.get_updates()
-        print('printing get_last_update result:')
+        print('printing GET_LAST_UPDATE result:')
         print(get_result)
-
+        print('Length GET_RESULT:')
+        print(len(get_result))
         if len(get_result) > 0:
             last_update = get_result[-1]
             print('1')
         else:
-            last_update = get_result[len(get_result)]
             print('2')
-        return last_update
+            print(get_result[len(get_result)])
+            last_update = get_result[len(get_result)]
         print('3')
+        return last_update
 
 
 greet_bot = BotHandler(token)
@@ -50,14 +54,18 @@ def main():
     while True:
         print('in WHILE statement...')
         greet_bot.get_updates(new_offset)
-        print('greetbot_get:')
-        print(greet_bot.get_updates(new_offset))
+        #print('greetbot_get:')
+        #print(greet_bot.get_updates(new_offset))
         last_update = greet_bot.get_last_update()
-
+        print('last_update')
+        print(last_update)
         last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+        # last_chat_text = last_update['message']['text']
+        # last_chat_id = last_update['message']['chat']['id']
+        # last_chat_name = last_update['message']['chat']['first_name']
+        last_chat_text = last_update['channel_post']['text']
+        print('LAST_CHAT_TEXT')
+        print(last_chat_text)
 
         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
             greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
